09715e6be75638e62dc64a80ac7c2b5c1c2ad60c
NV-Dev <Nothing-Dev@users.noreply.github.com>
2016-07-04 11:25:16 -0400

    Attemp to fix build.

--- du.mk
+++ du.mk
@@ -11,3 +11,5 @@ PRODUCT_NAME=serranoltexx
 TARGET_DEVICE=serranolte
 PRODUCT_NAME := du_serranoltexx
 PRODUCT_MODEL := GT-i9195
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/serranoltexx/serranolte:4.4.2/KOT49H/I9195XXUCNE6:user/release-keys PRIVATE_BUILD_DESC="serranoltexx-user 4.4.2 KOT49H I9195XXUCNE6 release-keys"
